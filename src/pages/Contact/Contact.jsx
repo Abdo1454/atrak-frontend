02756fc712c4669628029ad893494a6441109659
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-07-27 23:02:34 +0300

    feat(contact): add contact information section

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import HeroSection from "../../components/contact/HeroSection";
-// import ContactInfo from "../../components/contact/ContactInfo";
+import ContactInfo from "../../components/contact/ContactInfo";
 // import ContactForm from "../../components/contact/ContactForm";
 // import BusinessHours from "../../components/contact/BusinessHours";
 // import SocialLinks from "../../components/contact/SocialLinks";
@@ -11,8 +11,8 @@ function Contact() {
     <main dir="rtl" className="bg-gray-50">
       <div className="container mx-auto space-y-24 px-4 py-12">
         <HeroSection />
-{/* 
         <ContactInfo />
+{/* 
 
         <div className="grid gap-10 lg:grid-cols-2">
           <ContactForm />

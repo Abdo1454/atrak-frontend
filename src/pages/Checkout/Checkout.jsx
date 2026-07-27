a7bf4be39e301c84481ca00c6896858c6a19d0df
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-07-27 19:12:17 +0300

    feat(checkout): complete checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,31 +1,58 @@
-import BillingAddress from "../../components/checkout/BillingAddress";
 import CheckoutHeader from "../../components/checkout/CheckoutHeader";
 import ShippingForm from "../../components/checkout/ShippingForm";
+import BillingAddress from "../../components/checkout/BillingAddress";
 import ShippingMethod from "../../components/checkout/ShippingMethod";
-import PaymentMethod from "../../components/checkout/PaymentMethod"
+import PaymentMethod from "../../components/checkout/PaymentMethod";
 import OrderReview from "../../components/checkout/OrderReview";
+import OrderNotes from "../../components/checkout/OrderNotes";
 import OrderSummary from "../../components/checkout/OrderSummary";
 import PlaceOrderButton from "../../components/checkout/PlaceOrderButton";
-import OrderNotes from "../../components/checkout/OrderNotes";
-
 
 const Checkout = () => {
   return (
     <main className="min-h-screen bg-gray-50 px-4 py-8 md:px-8">
+
       <div className="mx-auto max-w-7xl">
 
         <CheckoutHeader />
-        <ShippingForm />
-        <BillingAddress />
-        <ShippingMethod />
-        <PaymentMethod />
-        <OrderReview />
-        <OrderSummary />
-        <div className="mt-6">
-  <PlaceOrderButton />
-<OrderNotes />
-</div>
+
+
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+
+
+          {/* Left Side */}
+          <div className="space-y-6 lg:col-span-2">
+
+            <ShippingForm />
+
+            <BillingAddress />
+
+            <ShippingMethod />
+
+            <PaymentMethod />
+
+            <OrderReview />
+
+            <OrderNotes />
+
+          </div>
+
+
+
+          {/* Right Side */}
+          <div className="space-y-6">
+
+            <OrderSummary />
+
+            <PlaceOrderButton />
+
+          </div>
+
+
+        </div>
+
       </div>
+
     </main>
   );
 };
